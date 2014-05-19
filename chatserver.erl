4086ec8e40506cc3e816b639d8b5a9a4f9aec21b
Philipp Neugebauer <ppn@nexgo.de>
2014-05-19 02:15:02 +0200

    chatserver supports now names

--- chatserver.erl
+++ chatserver.erl
@@ -1,6 +1,6 @@
 -module(chatserver).
 -export([startserver/0, chatserver/1]).
--export([login/1]).
+-export([login/2]).
 
 % start by variable_name = chatserver:startserver().
 startserver() ->
@@ -11,24 +11,29 @@ chatserver(Chatter) ->
     process_flag(trap_exit, true),
     receive
         {Sender, Message} ->
-        	case string:equal(Message, "login") of
-        		true -> io:format("loginmessage received\n"), link(Sender), chatserver([Sender|Chatter]);
-        		false -> io:format("message received\n"), lists:foreach(fun(N) -> N ! {Sender, Message, false} end, Chatter), chatserver(Chatter)
-            end;
+            io:format("message received\n"), 
+            lists:foreach(fun({Id,N}) -> Id ! {lists:filter(fun({I,Name}) -> Sender == I end, Chatter), Message, false} end, Chatter), 
+            chatserver(Chatter);
+        {Sender, Name, login} ->
+            io:format("loginmessage received\n"), 
+            link(Sender), 
+            chatserver([{Sender, Name}|Chatter]);
+        {'EXIT',From,Reason} ->
+            io:format("client ~p stopped\n", [From]),
+            unlink(From),
+            lists:delete(From,Chatter),
+            chatserver(Chatter);
         {Sender, Receiver, PersonalMessage} ->
             io:format("personalMessage received\n"),
         	Receiver ! {Sender, PersonalMessage, true},
         	chatserver(Chatter);
-        kill ->
-            lists:foreach(fun(N) -> (unlink(N)) and (N ! terminate) end, Chatter),
+        kick ->
+            io:format("server kicked all clients\n"),
+            lists:foreach(fun({I,N}) -> (unlink(I)) and (I ! terminate) end, Chatter),
             chatserver(Chatter);
         terminate ->
             io:format("server terminates\n"),
-            lists:foreach(fun(N) -> (unlink(N)) and (N ! terminate) end, Chatter);
-        {'EXIT',From,Reason} ->
-            unlink(From),
-            lists:delete(From,Chatter),
-            chatserver(Chatter);
+            lists:foreach(fun({I,N}) -> (unlink(I)) and (I ! terminate) end, Chatter);
         _ ->
             chatserver(Chatter)
     end.
@@ -38,18 +43,19 @@ chatter() ->
         terminate ->
             io:format("~p terminates\n",[self()]);
         {Sender, Message, true} ->
-        	io:format("~p: ~p\n",[Sender, Message]),
+        	io:format("~p to you: ~p\n",[Sender, Message]),
         	chatter();
         {Sender, Message, false} ->
-            io:format("ALL: ~p\n",[Message]),
+            io:format("~p to ALL: ~p\n",[Sender, Message]),
             chatter()
     end.
 
-login(Server) ->
+login(Server, Name) ->
 	Client = spawn(fun chatter/0),
-	Server ! {Client, "login"},
+	Server ! {Client, Name, login},
 	Client.
 
 logout(Client) ->
     Client ! terminate.
+
     
