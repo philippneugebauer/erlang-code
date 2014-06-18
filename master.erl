6e12f1c7dfb25682116871be0e62e1e888ff7aeb
Philipp Neugebauer <ppn@nexgo.de>
2014-06-19 00:12:46 +0200

    master example added

---
+++ master.erl
@@ -0,0 +1,38 @@
+-module(master).
+-export([master/1]).
+
+% start
+% Master = spawn(master,master,[[]]).
+
+master(Slaves) ->
+    process_flag(trap_exit,true),
+    receive
+        create ->
+            Child = spawn(fun slave/0),
+            link(Child),
+            master([Child|Slaves]);
+        {send, X} ->
+            lists:foreach(fun(N) -> N ! X end, Slaves),
+            master(Slaves);
+        kill ->
+            lists:foreach(fun(N) -> (unlink(N)) and (N ! terminate) end, Slaves),
+            master(Slaves);
+        terminate ->
+            lists:foreach(fun(N) -> (unlink(N)) and (N ! terminate) end, Slaves);
+        {'EXIT',From,Reason} ->
+            unlink(From),
+            lists:delete(From,Slaves),
+            self() ! create,
+            master(Slaves);
+        _ ->
+            master(Slaves)
+    end.
+
+slave() ->
+    receive
+        terminate ->
+            io:format("~p terminates\n",[self()]);
+        X ->
+            io:format("~p: ~p\n",[self(), X]),
+            slave()
+    end.
